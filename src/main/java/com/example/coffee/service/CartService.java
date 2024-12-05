0f3afe33e7073b1a21a448bdcd694c0a5b39c558
user <Yu-Hyeon-su>
2024-12-05 14:07:19 +0900

    장바구니 조회,추가,변경 기능 구현

--- src/main/java/com/example/coffee/service/CartService.java
+++ src/main/java/com/example/coffee/service/CartService.java
@@ -2,36 +2,112 @@ package com.example.coffee.service;
 
 import com.example.coffee.common.Result;
 import com.example.coffee.common.ResultCode;
-import com.example.coffee.model.cart.CartListDTO;
-import com.example.coffee.model.cart.CartDTO;
-import com.example.coffee.model.cart.ViewCartListDTO;
+import com.example.coffee.model.cart.*;
 import com.example.coffee.repository.CartMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class CartService {
     @Autowired
     private CartMapper cartMapper;
 
-    public Result viewCartList(CartDTO to) {
-        return new Result(ResultCode.SUCCESS, cartMapper.viewCartList(to));
+    public Result viewCartList(Long cartId) {
+        // 잘못된 요청
+        if (cartId == null || cartId <= 0) {
+            return new Result(ResultCode.INVALID_PARAMETER);
+        }
+
+        // 해당 아이디 존재 확인
+        if (cartMapper.selectCartId(cartId) == null) {
+            return new Result(ResultCode.USER_NOT_FOUND);
+        }
+
+        // 목록 없음
+        if (cartMapper.viewCartList(cartId).isEmpty()) {
+            return new Result(ResultCode.CART_NOT_FOUND);
+        }
+
+        // 정상 입력
+        return new Result(ResultCode.SUCCESS, cartMapper.viewCartList(cartId));
     }
 
-    public Result addProductToCart(CartListDTO to) {
-        cartMapper.addProductToCart(to);
+    public Result cartProductAdd(CartAddRequestDTO request) {
+        // 해당 아이디 존재 확인
+        if (cartMapper.selectCartId(request.getCartId()) == null) {
+            return new Result(ResultCode.USER_NOT_FOUND);
+        }
+
+        // 해당 상품 존재 확인
+        if (cartMapper.getProductById(request.getProductId()) == null) {
+            return new Result(ResultCode.PRODUCT_NOT_FOUND);
+        }
+
+        // 이미 추가된 상품인지 확인
+        int count = cartMapper.countProductByCartIdAndProductId(request);
+        if (count > 0) {
+            return new Result(ResultCode.ITEM_ALREADY_IN_CART);
+        }
+
+        // 해당 상품 재고 확인
+        if (cartMapper.getProductInventoryById(request.getProductId()) - request.getQuantity() < 0) {
+            return new Result(ResultCode.OUT_OF_STOCK);
+        }
+
+        // 정상 입력
+        Map<String, Object> params = new HashMap<>();
+        params.put("cartId", request.getCartId());
+        params.put("productId", request.getProductId());
+        params.put("quantity", request.getQuantity());
+
+        int price = (cartMapper.getPriceById(request.getProductId())) * request.getQuantity();
+        params.put("price", price);
+
+        cartMapper.cartProductAdd(params);
         return new Result(ResultCode.SUCCESS);
     }
 
-    public Result deleteCartList(CartListDTO to) {
-        cartMapper.deleteCartList(to);
-        return new Result(ResultCode.SUCCESS);
-    }
+    public Result updateCartList(CartUpdateRequestDTO request) {
+        // 해당 아이디 존재 확인
+        if (cartMapper.selectCartId(request.getCartId()) == null) {
+            return new Result(ResultCode.USER_NOT_FOUND);
+        }
 
-    public Result updateCartList(CartListDTO to) {
+        // 해당 아이디가 가지고 있는 상품인지 확인(X)
+        if (cartMapper.getProductById(request.getProductId()) == null) {
+            return new Result(ResultCode.PRODUCT_NOT_FOUND);
+        }
+
+        // 해당 상품 재고 없음
+        if (cartMapper.getProductInventoryById(request.getProductId()) - request.getQuantity() < 0) {
+            return new Result(ResultCode.OUT_OF_STOCK);
+        }
+
+        // 정상 입력
+        CartUpdateRequestDTO to = new CartUpdateRequestDTO();
+        to.setCartId(request.getCartId());
+        to.setProductId(request.getProductId());
+        to.setQuantity(request.getQuantity());
         cartMapper.updateCartList(to);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    public Result deleteCartList(CartDeleteRequestDTO request) {
+        // 해당 아이디 없음
+        if (cartMapper.selectCartId(request.getCartId()) == null) {
+            return new Result(ResultCode.USER_NOT_FOUND);
+        }
+
+        // 해당 아이디가 가지고 있는 상품인지 확인(X)
+        if (cartMapper.getProductById(request.getProductId()) == null) {
+            return new Result(ResultCode.PRODUCT_NOT_FOUND);
+        }
+
+        // 정상 입력
+        cartMapper.deleteCartList(request);
         return new Result(ResultCode.SUCCESS);
     }
 }
