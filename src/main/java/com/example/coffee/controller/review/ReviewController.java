02f3799888c392b8dcc7c602eb5a2af56e26ee5a
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-12-05 14:07:41 +0900

    feat: insert review

--- src/main/java/com/example/coffee/controller/review/ReviewController.java
+++ src/main/java/com/example/coffee/controller/review/ReviewController.java
@@ -1,4 +1,25 @@
 package com.example.coffee.controller.review;
 
+
+import com.example.coffee.common.Result;
+import com.example.coffee.model.review.ReviewDTO;
+import com.example.coffee.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/review")
 public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public Result insertReview(@AuthenticationPrincipal Long userId, @RequestBody ReviewDTO reviewDTO){
+        return reviewService.insertReview(userId, reviewDTO);
+    }
 }
