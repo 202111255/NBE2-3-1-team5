1b02c3b6a71e4457311bf47b1e50bf0bd4fb3b63
user <Yu-Hyeon-su>
2024-12-09 12:16:42 +0900

    url변경, dto클래스 축소

--- src/main/java/com/example/coffee/controller/cart/CartController.java
+++ src/main/java/com/example/coffee/controller/cart/CartController.java
@@ -1,10 +1,8 @@
 package com.example.coffee.controller.cart;
 
 import com.example.coffee.common.Result;
-import com.example.coffee.model.cart.CartDeleteRequestDTO;
-import com.example.coffee.model.cart.CartAddRequestDTO;
 
-import com.example.coffee.model.cart.CartUpdateRequestDTO;
+import com.example.coffee.model.cart.CartAddRequestDTO;
 import com.example.coffee.service.CartService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,14 +34,14 @@ public class CartController {
     }
 
     @Operation(summary = "장바구니 상품 개수 변경", description = "장바구니 상품 개수 변경 API 입니다. 요청항목 : 로그인 필수 / 상품(product) ID, 상품 개수" )
-    @PutMapping("/cart")
-    public Result updateCartList(@AuthenticationPrincipal Long userId, @RequestBody CartUpdateRequestDTO request) {
-        return cartService.updateCartList(userId, request);
+    @PutMapping("/cart/products/{productId}")
+    public Result updateCartList(@AuthenticationPrincipal Long userId, @PathVariable Long productId, @RequestParam int quantity) {
+        return cartService.updateCartList(userId, productId, quantity);
     }
 
     @Operation(summary = "장바구니 상품 삭제", description = "장바구니에서 상품 빼기 API 입니다. 요청항목 : 로그인 필수 / 상품(product) ID" )
-    @DeleteMapping("/cart")
-    public Result deleteCartList(@AuthenticationPrincipal Long userId, @RequestParam Long productId) {
+    @DeleteMapping("/cart/products/{productId}")
+    public Result deleteCartList(@AuthenticationPrincipal Long userId, @PathVariable Long productId) {
         return cartService.deleteCartList(userId, productId);
     }
 }
